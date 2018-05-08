0729247b86ae10ba6d52a1f41e704beb997bd2e3
Nic B <n1scby@yahoo.com>
2018-05-08 14:40:51 -0400

    vue menu

---
+++ menu.js
@@ -0,0 +1,11 @@
+var demo = new Vue({
+    el: '#main',
+    data: {
+        active: 'home'
+    },
+    methods: {
+        makeActive: function(item){
+            this.active = item;
+        }
+    }
+});
